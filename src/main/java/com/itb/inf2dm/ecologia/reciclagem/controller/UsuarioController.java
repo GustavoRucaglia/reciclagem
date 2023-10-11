00ae2719ac121d4a234fb0b4d75980e55229b899
Rayane Vieira <rayvieiraaa@gmail.com>
2023-10-11 17:57:51 -0300

    projeto finalizado

--- src/main/java/com/itb/inf2dm/ecologia/reciclagem/controller/UsuarioController.java
+++ src/main/java/com/itb/inf2dm/ecologia/reciclagem/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.itb.inf2dm.ecologia.reciclagem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itb.inf2dm.ecologia.reciclagem.model.Reciclador;
@@ -18,22 +23,43 @@ public class UsuarioController {
 		UsuarioRepository usuarioRepository;
 		
 		//páginas html
-		@GetMapping("/cadastro")
+		@GetMapping("/cadastroIntra")
 		  public String carrinho(Reciclador reciclador,Model model) {
-			
 			model.addAttribute("reciclador",reciclador);
-			return "cadastro";
+			return "intra";
 		}
 		
-		@PostMapping("/add-reciclador")
-		String addNovoReciglador(Reciclador reciclador, Model model) {
-			
-			reciclador.setCodStatusReciclador(true);
-			usuarioRepository.save(reciclador);
-			
-			return "redirect:/reciclagem/cadastro";
+		@GetMapping("/adm")
+		  public String adm(Reciclador reciclador,Model model) {
+			List<Reciclador> listaDeUser = usuarioRepository.findAll();
+			model.addAttribute("listaDeUser", listaDeUser);
+			return "adm";
+		}
 		
-		}	
+		@PostMapping("/addUsuario")
+		String addNovoReciglador(Reciclador reciclador, Model model) {
+		    usuarioRepository.save(reciclador);
+		    return "redirect:/reciclagem/adm";
+		}
 	    
+		@PutMapping("/desativar-usuario/{id}")
+		public ResponseEntity<String> desativarUsuario(@PathVariable Long id) {
+		    usuarioRepository.findById(id).ifPresent(usuario -> {
+		        usuario.setStatuscode(false);
+		        usuarioRepository.save(usuario);
+		    });
+
+		    return ResponseEntity.ok("Usuário desativado com sucesso");
+		}
+		
+		@PutMapping("/ativar-usuario/{id}")
+		public ResponseEntity<String> ativarUsuario(@PathVariable Long id) {
+		    usuarioRepository.findById(id).ifPresent(usuario -> {
+		        usuario.setStatuscode(true);
+		        usuarioRepository.save(usuario);
+		    });
+
+		    return ResponseEntity.ok("Usuário ativado com sucesso");
+		}
 
 }
--- src/main/java/com/itb/inf2dm/ecologia/reciclagem/model/Reciclador.java
+++ src/main/java/com/itb/inf2dm/ecologia/reciclagem/model/Reciclador.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_recilador")
+@Table(name = "tb_reciclador")
 public class Reciclador {
 	
 	@Id
@@ -16,78 +16,82 @@ public class Reciclador {
 	private String cep;
 	private String nome;
 	private String email;
-	private String senha;
 	private String cpf;
 	private String telefone;
-	private boolean codStatusReciclador;
+	private Boolean statuscode= true;
 	
+	public Reciclador() {
+		
+	}
 	
-
-	
-	
-	public Reciclador(Long id, String cep, String nome, String email, String senha, String cpf, String telefone,
-			boolean codStatusReciclador) {
+	public Reciclador(Long id, String cep, String nome, String email, String cpf, String telefone, Boolean statuscode) {
+		super();
 		this.id = id;
 		this.cep = cep;
 		this.nome = nome;
 		this.email = email;
-		this.senha = senha;
 		this.cpf = cpf;
 		this.telefone = telefone;
-		this.codStatusReciclador = codStatusReciclador;
 	}
-
-
-	public String getSenha() {
-		return senha;
-	}
-
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getCep() {
 		return cep;
 	}
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	public boolean getCodStatusReciclador() {
-		return codStatusReciclador;
+
+	public Boolean getStatuscode() {
+		return statuscode;
 	}
-	public void setCodStatusReciclador(boolean codStatusReciclador) {
-		this.codStatusReciclador = codStatusReciclador;
+
+	public void setStatuscode(Boolean statuscode) {
+		this.statuscode = statuscode;
 	}
+	
+	
+
+	
+	
 }
